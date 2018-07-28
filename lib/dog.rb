ccd55aeaa8cbc3ef342306e045b8a79fd26e4b71
Agatha Torku <torku.ae@gmail.com>
2018-07-28 03:34:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,15 +3,17 @@ class Dog
   def initialize(name)
     @name = name
   end
-  def name=(dog_name)
-    @dog_name = dog_name
+  def name=(name)
+    @name = name
   end
 
 #teach breed
 def initialize(breed)
   @breed = breed
 end
+
 def breed=(dog_breed)
   @dog_breed = dog_breed
 end
+
 end
